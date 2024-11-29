8bf76cd90896bbafd86de5cca0851f512f780233
Veerapat Sintupong <phantomgunorsoul@gmail.com>
2024-11-29 16:11:42 +0700

    add form

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, request, render_template
 import os
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 
-@app.route('/')
+def send_email(receiver_email, subject, body):
+    sender_email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+
+    try:
+        msg = MIMEText(body)
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+        msg['Subject'] = subject
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.send_message(msg)
+        return "Email sent!"
+    except Exception as e:
+        return f"Error: {e}"
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'POST':
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+        return send_email(email, subject, message)
+    return render_template('form.html')
 
 
 if __name__ == '__main__':
